77bb58af7209f64c2ef0ccc7f3a407775b7a47fe
José <jose.quirozquiroz93@gmail.com>
2026-02-28 10:32:28 -0500

    Refactor: eliminar duplicados y limpiar modularidad en módulo programas

--- frontend/src/modules/ofertas/views/OfertasView.vue
+++ frontend/src/modules/ofertas/views/OfertasView.vue
@@ -1,10 +1,14 @@
+
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold mb-4">Ofertas</h1>
-    <OfertasList />
+  <div :class="$style.root">
+    <h1 :class="$style.titulo">Ofertas</h1>
+    <div :class="$style.lista">
+      <OfertasList />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import OfertasList from './OfertasList.vue';
+import $style from './OfertasView.module.css';
 </script>
